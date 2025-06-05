71ad30ec33b3fb5b7aa568a0c6a4832a065f04e2
Sean Sundberg <seansund@us.ibm.com>
2025-06-05 00:54:23 -0500

    Update cluster login module in test
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage1-cluster.tf
+++ test/stages/stage1-cluster.tf
@@ -1,15 +1,8 @@
 module "dev_cluster" {
-  source = "github.com/cloud-native-toolkit/terraform-ibm-ocp-vpc.git"
+  source = "github.com/cloud-native-toolkit/terraform-ibm-cluster-login.git"
 
-  resource_group_name     = var.resource_group_name
-  region                  = var.region
-  ibmcloud_api_key        = var.ibmcloud_api_key
-  name                    = var.cluster_name
-  worker_count            = 2
-  name_prefix             = var.name_prefix
-  exists                  = true
-  cos_id                  = ""
-  vpc_subnet_count        = 1
-  vpc_name                = ""
-  vpc_subnets             = []
+  resource_group_name = var.resource_group_name
+  region = var.region
+  name = var.cluster_name
+  name_prefix = var.name_prefix
 }
